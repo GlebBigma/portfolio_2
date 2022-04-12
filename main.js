de1d835daf55ac284e282e0a4b423ba3370b6f6a
Gleb Bigma <glebbigma@gmail.com>
2022-04-12 18:11:43 +0300

    Finished main content, fixed scrollTo script.

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import './main.scss';
 import 'normalize.css/normalize.css';
+
+// Scroll to anchor
+document.querySelectorAll('a.scrollTo[href^="#"]')
+    .forEach(target =>
+        target.addEventListener("click", function (event) {
+            event.preventDefault();
+            document.querySelector(this.getAttribute("href"))
+                .scrollIntoView({behavior: "smooth"});
+        })
+    );
