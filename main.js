c351549fad3402b92b3ba5ffa007062545bcc4df
Gleb Bigma <glebbigma@gmail.com>
2022-05-02 21:14:43 +0300

    Added mixins, logic for siebar toggling, autoprefixer.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import './main.scss';
+import './mixins.scss'
 import 'normalize.css/normalize.css';
 
 // Scroll to anchor
@@ -10,3 +11,30 @@ document.querySelectorAll('a.scrollTo[href^="#"]')
                 .scrollIntoView({behavior: "smooth"});
         })
     );
+
+// Toggle sidebar on mobile and tablet
+const body = document.body;
+const burger = document.getElementById('btn-burger');
+const sidebar = document.getElementById('sidebar');
+const patrioticWrapper = document.getElementById('patrioticWrapper');
+const mainContent = document.getElementById('mainContent');
+
+const toggleSidebar = () => {
+    body.classList.toggle('overflow-hidden')
+    burger.classList.toggle('active');
+    sidebar.classList.toggle('active');
+    patrioticWrapper.classList.toggle('blurred');
+    mainContent.classList.toggle('blurred');
+};
+
+burger.addEventListener('click', toggleSidebar, false);
+
+// Close sidebar on mobile and tablet on main content click
+const closeSidebar = () => sidebar.classList.contains('active') && toggleSidebar();
+
+mainContent.addEventListener('click', closeSidebar, false);
+
+// Close sidebar on sidebar link click
+const sidebarNavs = document.querySelectorAll('.sidebar__nav-link');
+
+sidebarNavs.forEach(item => item.addEventListener('click', closeSidebar, false));
