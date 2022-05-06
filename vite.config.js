efa1fa323194a84153c69560d6f28d7c1a451604
Gleb Bigma <glebbigma@gmail.com>
2022-05-06 14:55:40 +0300

    Fixes for gh-pages.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-// export default {
-//     base: '/portfolio_2/'
-// }
-
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 
 export default defineConfig({
     base: '/portfolio_2/',
-    root: 'src',
     build: {
-        outDir: '../dist'
+        outDir: './docs',
+        emptyOutDir: true
     }
 })
