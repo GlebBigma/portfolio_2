af86ed19b07dc377db5bf787acca8ef1454f1c1f
Gleb Bigma <glebbigma@gmail.com>
2022-05-17 13:54:45 +0300

    Added loader.

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ import './main.scss';
 import './mixins.scss'
 import 'normalize.css/normalize.css';
 
+// Remove loader and add .log, after page loaded
+window.addEventListener('load', function () {
+    document.getElementById('loader-wrapper').classList.add('hidden');
+
+    console.log('РУССКИЙ ВОЕННЫЙ КОРАБЛЬ, ИДИ НА Х*Й');
+    console.log('RUSSIAN WARSHIP, GO F*CK YOURSELF');
+});
+
 // Scroll to anchor
 document.querySelectorAll('a.scrollTo[href^="#"]')
     .forEach(target =>
