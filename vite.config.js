341bb219ec4bf800c6f0986aa503a8da7a441300
Gleb Bigma <glebbigma@gmail.com>
2024-10-30 14:29:43 +0200

    Added Pug for test.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite';
+import pugPlugin from 'vite-plugin-pug';
+
+const options = { pretty: true }; // FIXME: pug pretty is deprecated!
+const locals = { name: 'My Pug' };
 
 export default defineConfig({
-    base: '/portfolio_2/',
-    build: {
-        outDir: './docs'
-    }
-})
+  base: '/portfolio_2/',
+  build: {
+    outDir: './docs',
+  },
+  plugins: [pugPlugin(options, locals)],
+});
